a8b532e7519724fba4e3eecdbde179726a52083f
Markus Yaseen <MarkusY@ppedv.de>
2019-04-15 16:22:38 +0200

    out und ref Beispiel

--- CSharp_Grundlagen/Program.cs
+++ CSharp_Grundlagen/Program.cs
@@ -10,19 +10,37 @@ namespace CSharp_Grundlagen
     {
         static void Main(string[] args)
         {
-            int[] zahlenListe = new int[] { 2, 5, 6, 1, 4 };
 
-            for (int i = 0; i < zahlenListe.Length; i++)
-            {
-                Console.WriteLine(zahlenListe[i]);
-            }
+            int x = 5;
+            // Bei ref muss die Variable vorher initialisiert werden
+            // Bei out muss die Variable nur deklariert werden
+            Addiere(5, 3, ref x);
+            Console.WriteLine(x);
 
-            foreach (var item in zahlenListe)
-            {
-                Console.WriteLine(item);
-            }
+            Console.WriteLine(Addiere(5, 3));
 
             Console.ReadKey();
+        }
+        
+        public static int Addiere(int a, int b)
+        {
+            return a+b;
+            Console.WriteLine("Das ist nicht sichtbar");
+        }
+
+       public static int Addiere(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        //mit out und ref können wir mehrere Rückgabewerte zurückgeben
+        public static void Addiere(int a, int b, ref int c)
+        {
+            // bei ref kann ich auf die Variable vor dem Setzen zugreifen
+            // bei out kann ich vor dem Setzen nicht auf die Variable zugreifen
+            Console.WriteLine(c);
+            c = a + b;
+            Console.WriteLine("Das ist sichtbar");
         }
     }
 }
