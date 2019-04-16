77f64b2b909f90a910fa373f35d033d0917621b4
Markus Yaseen <MarkusY@ppedv.de>
2019-04-16 15:22:58 +0200

    Liste mit Zugriff auf geerbte Klassen

--- CSharp_Grundlagen/Program.cs
+++ CSharp_Grundlagen/Program.cs
@@ -18,6 +18,44 @@ namespace CSharp_Grundlagen
             Console.WriteLine(g);
             Person person2 = g;
             person2.Laufen();
+
+            var personenListe = new List<Person>();
+            personenListe.Add(g);
+            personenListe.Add(p);
+            personenListe.Add(person2);
+
+            var gärtnerListe = new List<Gärtner>();
+            gärtnerListe.Add(g);
+
+            foreach(var item in personenListe)
+            {
+                item.Laufen();
+
+                //Wir müssen nur einmal auf item zugreifen
+                Gärtner gärtner = item as Gärtner;
+                if(gärtner != null)
+                {
+                    gärtner.Gießen();
+                }
+                //Bis C# 7.0 hier müssen wir zwei mal auf item zugreifen
+                if(item is Gärtner)
+                {
+                    
+                    Gärtner gärtner1 = (Gärtner)item;
+                    gärtner1.Gießen();
+                }
+                //Ab C# 7.0
+                if(item is Gärtner gärtner2)
+                {
+                    gärtner2.Gießen();
+                }
+            }
+
+            foreach(var item in gärtnerListe)
+            {
+                item.Gießen();
+            }
+
             Console.ReadKey();
         }
     }
