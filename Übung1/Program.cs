c840b9a0a4822ee7b05803e64f613026d28bb49a
Markus Yaseen <MarkusY@ppedv.de>
2019-04-16 11:34:20 +0200

    Übung TestResultat mit Testergebnis von 0-10

---
+++ Übung1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Übung1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int eingegebeneZahl;
+            int gültigerMaxBereich = 10;
+            int gültigerMinBereich = 0;
+
+            do
+            {
+                Console.WriteLine("Bitte Punktzahl eingeben:");
+                int.TryParse(Console.ReadLine(), out eingegebeneZahl);
+                //Überprüfe ob die eingegebene Zahl gültig ist
+                if(eingegebeneZahl <= gültigerMaxBereich && eingegebeneZahl >= gültigerMinBereich) {
+                    switch (eingegebeneZahl)
+                    {
+                        case 10:
+                            {
+                                Console.WriteLine("Ergebnis: Sehr gut");
+                                break;
+                            }
+                        case 9:
+                            {
+                                Console.WriteLine("Ergebnis: gut");
+                                break;
+                            }
+                        case 8:
+                            {
+                                Console.WriteLine("Ergebnis: Befriedigend");
+                                break;
+                            }
+                        case 7:
+                            {
+                                Console.WriteLine("Ergebnis: Ausreichend");
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("Ergebnis: Leider nicht genügend Punkte erreicht");
+                                break;
+                            }
+                    }                    
+                } else if(eingegebeneZahl == -1)
+                {
+                    //hier wird abgebrochen
+                } else
+                {
+                    Console.WriteLine("Ungültige Eingabe");
+                }
+ 
+            }
+            while (eingegebeneZahl != -1);
+            Console.ReadKey();
+        }
+    }
+}
