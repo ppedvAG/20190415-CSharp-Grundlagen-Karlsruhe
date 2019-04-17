129dbee9f35d0ed9fc968e232e582032ad8ceeab
Markus Yaseen <MarkusY@ppedv.de>
2019-04-17 16:09:39 +0200

    Dateien Schreiben (mit Using, und Emoji) und Lesen dieser Datei

---
+++ LesenSchreiben/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesenSchreiben
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter sw = new StreamWriter("Datei.txt"))
+            {
+                var emoji = char.ConvertFromUtf32(0x1F610);
+                sw.WriteLine("Zeile1\u24B7\u28A9" + emoji);
+                sw.WriteLine("Zeile2");
+                sw.WriteLine("Zeile3");
+            }
+
+            StreamReader sr = new StreamReader("Datei.txt");
+            while(!sr.EndOfStream)
+            {
+                string zeile = sr.ReadLine();
+            }
+            sr.Close();
+        }
+    }
+}
