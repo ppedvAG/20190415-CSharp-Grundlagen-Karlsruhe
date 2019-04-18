8f34771f4015344651f78ab3f303a63f13db06dc
Markus Yaseen <MarkusY@ppedv.de>
2019-04-18 10:02:37 +0200

    Füge den Finally Block zur Try Catch hinzu

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -16,17 +16,21 @@ namespace Exceptions
 
             try
             {
-                auto.Farbe = "Grün";
+                auto.Farbe = "Rot"; // Das wirft eine Exception
             }
             catch (FarbException e)
             {
-                Console.WriteLine("Bitte gib eine neue Farbe ein:");
+                Console.WriteLine("Bitte gib eine neue Farbe ein:"); // Wenn wir was anderes als Rot eingeben, haben wir ein Problem
                 auto.Farbe = Console.ReadLine();
                 Console.WriteLine($"Eine FarbException wurde abgefangen! Msg: {e.Message}");
             }
             catch(Exception e)
             {
                 Console.WriteLine("Exception");
+            } finally
+            {
+                Console.WriteLine("Der Finally-Block wird ausgeführt");
+                // Hier könnte man den Datenstream schließen (wenn es z.B. eine Datei wäre)
             }
 
             Console.WriteLine($"Die Farbe vom Auto ist: {auto.Farbe}!");
@@ -37,7 +41,7 @@ namespace Exceptions
             } catch(FormatException e)
             {
                 Console.WriteLine("Eine FormatException wurde abgefangen!");
-            } catch(Exception e)
+            } catch(Exception e) // Jede Exception wird hier abgefangen
             {
                 Console.WriteLine("Eine Exception wurde abgefangen!");
             }
