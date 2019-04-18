68d8ec6771a0be1b9b3a8956ec8d0f04de92ed18
Markus Yaseen <MarkusY@ppedv.de>
2019-04-18 08:52:40 +0200

    Fange eine Exception ab mit try catch

---
+++ Exceptions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte gib eine Zahl ein:");
+            int eingegebeneZahl;
+
+            try
+            {
+                eingegebeneZahl = int.Parse(Console.ReadLine());
+            } catch(Exception e)
+            {
+                Console.WriteLine("Eine Exception wurde abgefangen!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
